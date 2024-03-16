b9f0e76431823236c4d74a41aa3547051b3a9557
Gedeon Nzemba <40776419+GedeonNzemba@users.noreply.github.com>
2024-03-16 13:25:06 +0200

    updated db

--- src/models/Space.ts
+++ src/models/Space.ts
@@ -2,62 +2,64 @@
 import mongoose, { Document, Schema } from "mongoose";
 
 interface Principal {
-    pastor: string;
-    wife: string;
-    image: string;
-    description: string;
-  }
-  
-  interface Deacon {
-    names: string;
-    descriptions: string;
-    image: string;
-  }
-  
-  interface Trustee {
-    names: string;
-    descriptions: string;
-    image: string;
-  }
-  
-  interface Securities {
-    deacons: Deacon[];
-    trustees: Trustee[];
-  }
-  
-  interface LiveService {
-    title: string;
-    preacher: string;
-    sermon: string;
+  pastor: string;
+  wife: string;
+  image: string;
+  description: string;
 }
-  
+
+interface Deacon {
+  names: string;
+  descriptions: string;
+  image: string;
+}
+
+interface Trustee {
+  names: string;
+  descriptions: string;
+  image: string;
+}
+
+interface Securities {
+  deacons: Deacon[];
+  trustees: Trustee[];
+}
+
+interface LiveService {
+  title: string;
+  preacher: string;
+  sermon: string;
+}
+
 interface Songs {
-    title: string;
-    songUrl: string;
-  }
-  
+  title: string;
+  songUrl: string;
+}
+
 export interface ChurchDoc extends Document {
-    name: string;
-    principal: Principal;
-    location: string;
-    image: string;
-    banner: string[];
-    securities: Securities;
-    oldServices: LiveService[];
-    gallery: string[];
-    songs: Songs[];
-  }
+  name: string;
+  principal: Principal;
+  location: string;
+  image: string;
+  banner: string[];
+  securities: Securities;
+  oldServices: LiveService[];
+  gallery: string[];
+  songs: Songs[];
+  logo: string;
+}
 
 const ChurchSchema: Schema = new Schema({
-    name: { type: String, required: true },
-    principal: { type: Object, required: true },
-    location: { type: String, required: true },
-    image: { type: String, required: true },
-    banner: { type: [String], required: true },
-    securities: { type: Object, required: true },
-    oldServices: { type: [Object], required: true },
-    gallery: { type: [String], required: true },
-    songs: { type: [Object], required: true },
+  name: { type: String, required: true },
+  principal: { type: Object, required: true },
+  location: { type: String, required: true },
+  image: { type: String, required: true },
+  banner: { type: [String], required: true },
+  securities: { type: Object, required: true },
+  oldServices: { type: [Object], required: true },
+  gallery: { type: [String], required: true },
+  songs: { type: [Object], required: true },
+  logo: { type: String },
 });
 
-export const ChurchModel = mongoose.model<ChurchDoc>('Church', ChurchSchema);
+export const ChurchModel = mongoose.model<ChurchDoc>("Church", ChurchSchema);
--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,13 +1,10 @@
 // src/utils/db.ts
 import mongoose, { ConnectOptions } from "mongoose";
 
-const MONGODB_URI =
-  "mongodb+srv://saintshub:SaintsHub@App@cluster0.cxgzddn.mongodb.net/?retryWrites=true&w=majority";
-
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://capetowntabernacle:EmqmrTUTE9uA9geZ@cluster0.4doa5l1.mongodb.net/?retryWrites=true&w=majority",
+      "mongodb+srv://nzemba48:KrjaECTCuSSwKdFS@cluster0.cxgzddn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -20,3 +17,24 @@ const connectDB = async () => {
 };
 
 export default connectDB;
+// import mongoose, { ConnectOptions } from "mongoose";
+
+// const MONGODB_URI =
+//   "mongodb+srv://saintshub:SaintsHub@App@cluster0.cxgzddn.mongodb.net/?retryWrites=true&w=majority";
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(
+//       "mongodb+srv://capetowntabernacle:EmqmrTUTE9uA9geZ@cluster0.4doa5l1.mongodb.net/?retryWrites=true&w=majority",
+//       {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//       } as ConnectOptions
+//     );
+//     console.log("Database is connected");
+//   } catch (error: any) {
+//     console.log(error.message);
+//   }
+// };
+
+// export default connectDB;
