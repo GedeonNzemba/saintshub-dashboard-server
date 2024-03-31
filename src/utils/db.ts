1cd6508c033d3bad870173c4817506ad45c77423
Gedeon Nzemba <40776419+GedeonNzemba@users.noreply.github.com>
2024-03-31 20:00:43 +0200

    updated server

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,31 +1,10 @@
 // src/utils/db.ts
-import mongoose, { ConnectOptions } from "mongoose";
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect(
-      "mongodb+srv://nzemba48:KrjaECTCuSSwKdFS@cluster0.cxgzddn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      } as ConnectOptions
-    );
-    console.log("Database is connected");
-  } catch (error: any) {
-    console.log(error.message);
-  }
-};
-
-export default connectDB;
 // import mongoose, { ConnectOptions } from "mongoose";
-
-// const MONGODB_URI =
-//   "mongodb+srv://saintshub:SaintsHub@App@cluster0.cxgzddn.mongodb.net/?retryWrites=true&w=majority";
 
 // const connectDB = async () => {
 //   try {
 //     await mongoose.connect(
-//       "mongodb+srv://capetowntabernacle:EmqmrTUTE9uA9geZ@cluster0.4doa5l1.mongodb.net/?retryWrites=true&w=majority",
+//       "mongodb+srv://nzemba48:KrjaECTCuSSwKdFS@cluster0.cxgzddn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
 //       {
 //         useNewUrlParser: true,
 //         useUnifiedTopology: true,
@@ -38,3 +17,24 @@ export default connectDB;
 // };
 
 // export default connectDB;
+import mongoose, { ConnectOptions } from "mongoose";
+
+const MONGODB_URI =
+  "mongodb+srv://saintshub:SaintsHub@App@cluster0.cxgzddn.mongodb.net/?retryWrites=true&w=majority";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://capetowntabernacle:EmqmrTUTE9uA9geZ@cluster0.4doa5l1.mongodb.net/?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      } as ConnectOptions
+    );
+    console.log("Database is connected");
+  } catch (error: any) {
+    console.log(error.message);
+  }
+};
+
+export default connectDB;
